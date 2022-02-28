652ffac41a921ce952800b1cee0237e2fd713f3f
K2FA <khalidarrafi6331@gmail.com>
2022-02-28 23:24:06 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [HomeController::class, 'home']);
+
+Route::prefix('category')->group(function(){
+    Route::get('/marbel-edu-games', function(){
+        echo 'Halaman ini menampilkan Marbel Education Games Page';
+    });
+    Route::get('/marbel-and-friends-kids-games', function(){
+        echo 'Halaman ini menampilkan Marbel and Friends Kids Games Page';
+    });
+    Route::get('/riri-story-books', function(){
+        echo 'Halaman ini menampilkan Riri Story Books Page';
+    });
+    Route::get('/kolak-kids-songs', function(){
+        echo 'Halaman ini menampilkan Kolak Kids Songs Page';
+    });
 });
